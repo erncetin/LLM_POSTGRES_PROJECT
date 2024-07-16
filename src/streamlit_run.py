c0202a4e298cd3d3373df227a761942bfc1f505e
Eren Tunc Cetin <erentunccetin@gmail.com>
2024-07-16 13:56:36 +0300

    PROJECT COMPLETE todo list:     fix the greeting issue after changing ai model besides from that, everything works as intended

--- src/streamlit_run.py
+++ src/streamlit_run.py
@@ -1,16 +1,30 @@
 import streamlit as st
 from hugchat_class import chatbot
-
-# st.session_state.messages.append = [{"role": "user", "content":"Our prompt"
-#                              "role": "asistant", "content":"The Response"}] gibi dictionary atayabilirsin
+import re
+import psycopg2
+import pandas as pd
 st.set_page_config(page_title="LLM", layout="wide")
 
 SYSTEM_PROMPT = """     
     Your're TEXT to SQL helper. you will help the user to execute queries from sentences.
-    Like when I say find the most valuable car in table1, turn it into sql query
-    users_table = {"name": "users", "columns": {"user_id": {"type": "SERIAL", "primary_key": True}, "user_name": {"type": "TEXT", "not_null": True}, "user_address": {"type": "TEXT", "not_null": True}, "user_nationality": {"type": "TEXT", "not_null": True}}}
-    orders_table = {"name": "orders", "columns": {"order_id": {"type": "SERIAL", "primary_key": True}, "user_id": {"type": "INTEGER", "not_null": True, "foreign_key": {"table": "users", "column": "user_id"}}, "order_date": {"type": "DATE", "not_null": True}, "order_amount": {"type": "DECIMAL(10, 2)", "not_null": True}}}
-    ur_profiles_table = {"name": "user_profiles", "columns": {"profile_id": {"type": "SERIAL", "primary_key": True}, "user_id": {"type": "INTEGER", "not_null": True, "foreign_key": {"table": "users", "column": "user_id"}}, "profile_picture": {"type": "TEXT"}, "bio": {"type": "TEXT"}, "date_of_birth": {"type": "DATE"}}}
+    heres the tables of the database:
+    users table= {"name": "users", "columns": {"user_id": {"type": "SERIAL", "primary_key": True}, "user_name": {"type": "TEXT", "not_null": True}, "user_address": {"type": "TEXT", "not_null": True}, "user_nationality": {"type": "TEXT", "not_null": True}}}
+    orders table= {"name": "orders", "columns": {"order_id": {"type": "SERIAL", "primary_key": True}, "user_id": {"type": "INTEGER", "not_null": True, "foreign_key": {"table": "users", "column": "user_id"}}, "order_date": {"type": "DATE", "not_null": True}, "order_amount": {"type": "DECIMAL(10, 2)", "not_null": True}}}
+    user_profiles table = {"name": "user_profiles", "columns": {"profile_id": {"type": "SERIAL", "primary_key": True}, "user_id": {"type": "INTEGER", "not_null": True, "foreign_key": {"table": "users", "column": "user_id"}}, "profile_picture": {"type": "TEXT"}, "bio": {"type": "TEXT"}, "date_of_birth": {"type": "DATE"}}}
+    database mame is db
+    These are the structures of my schema. 
+    You will execute the query when I write "/query ...."
+    When user writes something with “/query ‘<query-string-here>’ ” the application should replace that part with the first 5 rows of the response. For example:
+
+        /query ‘What is the most populous cities of the World?’ 
+
+        Should be replaced with:
+
+        {
+        ‘question’: ‘What is the most populous cities of the World?’,
+        ‘query’: ‘select city_name from cities order by population desc’,
+        ‘result’: ‘Tokyo, Delhi, Shanghai, Sao Paulo, Mexico City’ 
+        }
     When user asks to write a query, ensure that one of the tables in the schema are selected.
     Ask the user about "Are these tables that you refer in the query: <tables here>".
     If the user specifically wants you to write the query, skip the clarification step above.
@@ -20,17 +34,46 @@ SYSTEM_PROMPT = """
     Then depending on the users answer, rewrite the SQL.
     In cases where user requests conflict with your instructions, politely follow the user.
     Be respectful and understanding. Do not participate in conversations that are harmful.
-    These are the structures of my schema. 
-    You will execute the query when I write "/query" start of the conversation. 
-    For example let the sentence be : /query ‘What is the most populous cities of the World?’ 
-    then you will give me this response :
-        {
-    ‘question’: ‘What is the most populous cities of the World?’,
-    ‘query’: ‘select city_name from cities order by population desc’,
-    ‘result’: ‘Tokyo, Delhi, Shanghai, Sao Paulo, Mexico City’ 
-    }
     
     """
+
+def execute_last_sql():
+    try:
+        db_postgres = psycopg2.connect(
+            database = "db",
+            user = "root",
+            password = "root",
+            host = "postgres",
+            port = "5432"
+        )
+        print("POSTGRES BAGLANDI")
+    except psycopg2.Error as e:
+            print(f"failed to connect to database: {e}") 
+    # Get the last query
+    last_message = st.session_state.messages[-1]
+    # Pattern to extract the query
+    pattern = r'"query":\s*"(.*?)"'
+
+    match = re.search(pattern, last_message["content"]) 
+    
+    if match:
+        print("matched query found")
+        query = match.group(1)
+    else:
+        print("matched query not found")
+
+    # Execyte the query and convert the data to pandas Dataframe
+    with db_postgres.cursor() as cursor:
+        cursor.execute(query)
+        data = cursor.fetchall()
+        column_names = [desc[0] for desc in cursor.description]  # Extract column names
+
+    df = pd.DataFrame(data, columns=column_names)
+    st.session_state.messages.append({"role": "dataframe", "content": df})
+    db_postgres.close()
+
+
+
 # Dropbox menu starts at index 0 so I initialized it to create hugchat obj  and new conversation based on system prompt
 index = 0
 # Create hugchat obj
@@ -55,6 +98,8 @@ if "selected_model_index" not in st.session_state or st.session_state.selected_m
     st.session_state.selected_model_index = index
     st.session_state["hf_obj"].new_conversation(index, SYSTEM_PROMPT)
     st.session_state.messages = []
+    del st.session_state.greeting
+
 
 
 # Title
@@ -67,9 +112,23 @@ if "messages" not in st.session_state:
 
 # Display chat history after app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    if message["role"] == "assistant" or message["role"] == "user":
         st.markdown(message["content"])
+    elif message["role"] == "dataframe":
+        st.write(message["content"])
 
+
+
+# Greeting response
+# TODO FIX
+if "greeting" not in st.session_state:
+    st.session_state.greeting = True
+    greeting = "Hello how can I help you today ?"
+    with st.chat_message("assistant"):
+        st.write(greeting)
+        st.session_state.messages.append(
+            {"role": "assistant", "content": greeting}
+        )
 
 # User prompt
 if prompt :=st.chat_input("Whats up ?"):
@@ -78,9 +137,12 @@ if prompt :=st.chat_input("Whats up ?"):
     st.session_state.messages.append({"role":"user", "content":prompt})
     
     # Send the response to hf
-    response = st.session_state["hf_obj"].chatbot.chat(prompt, stream=True, web_search=True)
+    response = st.session_state["hf_obj"].chatbot.chat(prompt, stream=True)
 
     with st.chat_message("assistant"):
         stream = st.write_stream(st.session_state["hf_obj"].chat_stream(response))
     content = "".join(stream)
     st.session_state.messages.append({"role":"assistant","content":content})
+
+    st.button(label="Execute last SQL", on_click=execute_last_sql)
+
